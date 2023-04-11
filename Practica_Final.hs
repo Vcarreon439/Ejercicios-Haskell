aaec800746f373660b4ce7d36752c9eedc1ff6af
Víctor Hugo Carreón Pulido <192310436@itslerdo.edu.mx>
2023-04-10 19:02:48 -0600

    Update Practica_Final.hs

--- Practica_Final.hs
+++ Practica_Final.hs
@@ -1,26 +1,36 @@
 import Data.List (foldl')
 
+--Victor Hugo Carreon Pulido - 192310436
+--Edgar Eduardo Arguijo Vazquez - 192310252
+
+--https://www.glc.us.es/~jalonso/vestigium/i1m2014-ejercicios-sobre-arboles-binarios-en-haskell/
+
+--Para definir el arbol
 data Tree a = Empty | Node a (Tree a) (Tree a)
 
+--Para insertar un elemento en el arbol
 insert :: Ord a => a -> Tree a -> Tree a
 insert x Empty = Node x Empty Empty
 insert x (Node y left right)
   | x < y = Node y (insert x left) right
   | otherwise = Node y left (insert x right)
 
+--Para construir el arbol
 buildTree :: Ord a => [a] -> Tree a
 buildTree = foldl' (flip insert) Empty
 
+--Para imprimir el arbol
 instance Show a => Show (Tree a) where
   show Empty = "Vacio"
   show (Node x left right) = "Nodo " ++ show x ++ " (" ++ show left ++ ") (" ++ show right ++ ")"
 
+--Flujo principal del programa
 main :: IO ()
 main = do
   let nums = []
   input nums
 
-
+--Para leer los numeros en funcion de n
 input::[String]->IO()
 input xs = do
   print("Introduce un numero o (n/N) para salir")
@@ -28,6 +38,8 @@ input xs = do
   if x == "n" || x == "N"
     then do
       (print (buildTree(convertir(xs))))
+      print((inorden (buildTree(convertir(xs)))))
+      --Generame la funcion inorden
       print("Deseas volver a ejecutar el programa? (s/n)")
       y <- getLine
       if y == "s" || y == "S"
@@ -38,5 +50,11 @@ input xs = do
       return()
     else (input (xs++[x]))
 
+--Para convertir de String a Int
 convertir :: [String] -> [Int]
 convertir xr = map read xr 
+
+--Para ordenar el arreglo
+inorden :: Tree a -> [a]
+inorden Empty = []
+inorden (Node x left right) = inorden left ++ [x] ++ inorden right
